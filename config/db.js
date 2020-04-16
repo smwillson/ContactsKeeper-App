26b2c2728336b43272976c4e1d4a1ed13a0d0a91
Sama Mushtaq <sama.mushtaq@rgare.com>
2020-04-16 16:11:46 -0500

    added database connections using Mongoose

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully!");
+  } catch (err) {
+    console.error(`Failed to connect: ${err.message}`);
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
